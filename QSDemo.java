77025d25e7e4e5925570a4dffa3e41b7ecd2ec91
Luke <hodnett53@gmail.com>
2015-08-23 15:39:25 -0500

    add QSDemo class and compile

--- QSDemo.java
+++ QSDemo.java
@@ -29,3 +29,27 @@ class Quicksort {
     if(i < right) qs(items, i , right);
   }
 }
+
+class QSDemo {
+  public static void main(String args[]) {
+    char a[] = {'d', 'x', 'a', 'r', 'p', 'j', 'i'};
+    int i;
+
+    System.out.print("Original array: ");
+    for(i=0; i < a.length; i++) {
+      System.out.print(a[i]);
+    }
+
+    System.out.println();
+
+    // sort the array
+    Quicksort.qsort(a);
+
+    System.out.print("Sorted array: ");
+    for(i=0; i < a.length; i++) {
+      System.out.print(a[i]);
+    }
+
+    System.out.println();
+  }
+}
